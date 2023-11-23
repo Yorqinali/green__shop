c60c6cf64b821d54e8a0997cf9120ac5224c0504
Yorqinali <grose20030916@gmail.com>
2023-11-23 11:49:50 +0500

    greenshop

---
+++ js/categories.js
@@ -0,0 +1,38 @@
+const categories = [
+    {
+        title: "House Plants",
+        count: 33,
+    },
+    {
+        title: "Potter Plants",
+        count: 12,
+    },
+    {
+        title: "Seeds",
+        count: 65,
+    },
+    {
+        title: "Small Plants",
+        count: 39,
+    },
+    {
+        title: "Big Plants",
+        count: 23,
+    },
+    {
+        title: "Succulents",
+        count: 17,
+    },
+    {
+        title: "Trerrariums",
+        count: 19,
+    },
+    {
+        title: "Gardening",
+        count: 13,
+    },
+    {
+        title: "Accessories",
+        count: 18,
+    },
+]
---
+++ js/mockData.js
@@ -0,0 +1,49 @@
+const greenShopPlants = {
+  allPlants: [
+    {
+      img: "../assets/images/flower1.png",
+      title: "Barberton Daisy",
+      price: "$119.00",
+    },
+    {
+        img: "../assets/images/flower3.png",
+        title: "Chinese Evergreen",
+        price: "$232.00",
+    },
+    {
+      img: "../assets/images/flower2.png",
+      title: "African Violet",
+      price: "$119.00",
+    },
+    {
+      img: "../assets/images/flower8.jpg",
+      title: "Ramashka",
+      price: "$742.00",
+    },
+    {
+      img: "../assets/images/flower1.png",
+      title: "Barberton Daisy",
+      price: "$119.00",
+    },
+    {
+      img: "../assets/images/flower1.png",
+      title: "Barberton Daisy",
+      price: "$119.00",
+    },
+    {
+      img: "../assets/images/flower2.png",
+      title: "African Violet",
+      price: "$119.00",
+    },
+    {
+      img: "../assets/images/flower8.jpg",
+      title: "Ramashka",
+      price: "$742.00",
+    },
+    {
+      img: "../assets/images/flower3.png",
+      title: "Chinese Evergreen",
+      price: "$232.00",
+    },
+  ],
+};
